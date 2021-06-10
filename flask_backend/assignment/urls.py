2834176f1152637fba495a837017647dcaad6ceb
kushalkanavi <kushalkanavi@gmail.com>
2021-06-10 19:53:09 +0530

    Second Commit

--- flask_backend/assignment/urls.py
+++ flask_backend/assignment/urls.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
-from .views import Assignment
+from .views import FlaskAssignment, GetAssignmentByID, GetAssignmentByTag
 from flask_restful import Api
 
 assignment = Blueprint('assignment', __name__)
 api = Api(assignment)
 
-api.add_resource(Assignment, '/')
+api.add_resource(FlaskAssignment, '/')
+api.add_resource(GetAssignmentByID, '/<int:id>')
+api.add_resource(GetAssignmentByTag, '/assignmentbytag')
